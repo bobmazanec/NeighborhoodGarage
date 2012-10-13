f1f406a9c5b81c1ce7a0bb94682612db0c90753c
Bob Mazanec <bob@bobmazanec.com>
2012-10-13 08:54:47 -0500

    Remove NGFuelingDetailVC:viewDidLoad
    viewWillAppear: is sufficient for both initial (e.g., from NGFListVC) &
    update (e.g., after editing in NGFVC)

--- mobile/Neighborhood Garage/NGFuelingDetailViewController.m
+++ mobile/Neighborhood Garage/NGFuelingDetailViewController.m
@@ -44,13 +44,6 @@
     }
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    [self populateView];
-}
-
 - (void) viewWillAppear:(BOOL)animated {
     [self populateView];
 }
